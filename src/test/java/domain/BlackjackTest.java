8af26d9d8e429390b5c412a3aa90cdbba0f34daf
outstandingboy <outstanding1301@gmail.com>
2021-01-14 21:54:53 +0900

    test: add turn test

--- src/test/java/domain/BlackjackTest.java
+++ src/test/java/domain/BlackjackTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import domain.card.*;
 import domain.controller.GameController;
+import domain.controller.InputManager;
+import domain.controller.InputManagerImpl;
 import domain.repository.PlayerRepository;
 import domain.repository.PlayerRepositoryImpl;
 import domain.user.Player;
@@ -9,21 +11,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BlackjackTest {
     PlayerRepository repository;
     List<Card> cards;
+    GameController controller;
+
     @BeforeEach
     void init() {
         repository = new PlayerRepositoryImpl();
         repository.addPlayer(new Player("Ingeol", 100));
-        repository.addPlayer(new Player("Cat", 10000));
-        repository.addPlayer(new Player("Dog", 100));
-        repository.addPlayer(new Player("Pizza", 50));
+//        repository.addPlayer(new Player("Cat", 10000));
+//        repository.addPlayer(new Player("Dog", 100));
+//        repository.addPlayer(new Player("Pizza", 50));
 
         cards = CardFactory.create();
+
+        controller = new GameController(repository.getPlayers(), new InputManager() {
+            @Override
+            public boolean choiceState(Player player) {
+                player.printHand();
+                player.hit();
+                return true;
+            }
+        });
+        controller.init();
+        controller.printStatus();
     }
 
     @Test
@@ -50,9 +66,11 @@ public class BlackjackTest {
     }
 
     @Test
-    void gameTest() {
-        GameController controller = new GameController(repository.getPlayers());
-        controller.init();
-        controller.printStatus();
+    void choiceTest() {
+        controller.turn();
+        controller.turn();
+        controller.turn();
+        controller.turn();
+        controller.turn();
     }
 }
