5a679a53b38d40fff7a2c84334df8ac712be90c0
outstandingboy <outstanding1301@gmail.com>
2021-01-14 21:53:29 +0900

    feat: add getCardString

--- src/main/java/domain/card/Card.java
+++ src/main/java/domain/card/Card.java
@@ -15,8 +15,6 @@ public class Card {
         this.type = type;
     }
 
-    // TODO Card 관련 추가 기능 구현
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -41,5 +39,9 @@ public class Card {
 
     public Symbol getSymbol() {
         return symbol;
+    }
+
+    public String getCardString() {
+        return "["+type.getSymbol()+symbol.getScore()+"]";
     }
 }
